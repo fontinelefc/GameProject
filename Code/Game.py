894b891211310317d96b3d7eba9947ab8d387e13
fontinele <filipe2cerqueira@gmail.com>
2025-03-20 17:11:07 -0300

    Mode in Menu: config options, add responsiveness to Menu.

--- Code/Game.py
+++ Code/Game.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import pygame
+
+from Code.Const import MENU_OPTION
+from Code.Level import Level
 from Code.Menu import Menu
 
 class Game:
@@ -11,7 +14,14 @@ class Game:
     def run(self, ):
         while True:
             menu = Menu(self.window)
-            menu.run()
-            pass
+            menu_return = menu.run()
+
+            if menu_return == MENU_OPTION[0]:
+                level = Level(self.window, 'Level1', menu_return)
+            elif menu_return == MENU_OPTION[2]:
+                pygame.quit()  # close window
+                quit()  # end pygame
+            else:
+                pass
 
 
--- Code/Menu.py
+++ Code/Menu.py
@@ -14,6 +14,7 @@ class Menu:
         self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self, ):
+        menu_option = 0
         pygame.mixer_music.load('./asset/sound_menu.wav')
         pygame.mixer_music.play(-1)
         while True:
@@ -22,14 +23,31 @@ class Menu:
             self.menu_text(text_size=80, text="Scape", text_color=(255, 54, 69), text_center_pos=(288, 125))
 
             for i in range(len(MENU_OPTION)):
-                self.menu_text(text_size=40, text=MENU_OPTION[i],text_color=(225, 255, 255), text_center_pos=(288, 190 + 30 * i))
-
+                if i == menu_option:
+                    self.menu_text(text_size=40, text=MENU_OPTION[i],text_color=(10, 32, 66), text_center_pos=(288, 190 + 30 * i))
+                else:
+                    self.menu_text(text_size=40, text=MENU_OPTION[i], text_color=(225, 255, 255), text_center_pos=(288, 190 + 30 * i))
             pygame.display.flip()
 
             for event in pygame.event.get():
                    if event.type == pygame.QUIT:
                        pygame.quit() # close window
                        quit() # end pygame
+                   if event.type == pygame.KEYDOWN:
+                       if event.key == pygame.K_DOWN: # verificação de tecla seta para baixo
+                           if menu_option < len(MENU_OPTION) - 1:
+                               menu_option += 1
+                           else:
+                               menu_option = 0
+
+                       if event.key == pygame.K_UP: # verificação de tecla seta para cima
+                           if menu_option > 0:
+                               menu_option -= 1
+                           else:
+                               menu_option = len(MENU_OPTION) - 1
+
+                       if event.key == pygame.K_RETURN: # Enter
+                           return MENU_OPTION[menu_option]
 
     def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
         text_font: Font = pygame.font.SysFont("Lucida Sans Typewrite", size=text_size)
